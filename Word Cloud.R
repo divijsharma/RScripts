0ae3a5392a1d9dcf138a3d0bd8ab40e90a45ff8d
Divij Sharma <37137249+divijsharma@users.noreply.github.com>
2018-04-24 18:22:58 +0530

    WordCloud - Corpus, TDM, wordcloud & wordcloud2
    Added code for wordcloud2 package also to create wordcloud of various shapes.

--- Word Cloud.R
+++ Word Cloud.R
@@ -10,13 +10,15 @@ library(SnowballC)
 # Library for creating word cloud
 library(RColorBrewer)
 library(wordcloud)
+library(wordcloud2) # Wordcloud2 is a better packages that has much more flexibility on
+# shape of wordcloud. See also https://cran.r-project.org/web/packages/wordcloud2/vignettes/wordcloud.html
 
 ########## Read data ##########
 # By choosing stringsAsFactors = FALSE the text is read as text and not 
 # converted to factors. The default option is stringsAsFactors = TRUE because
 # of which the text is converted to factors and it becomes difficult to 
 # manipulate the data
-input_data <- read.csv(file.choose(), header = TRUE, stringsAsFactors = FALSE)
+input_data <- read.csv(file = "C:\\Users\\dsharma\\Desktop\\R\\Test Files\\JEOPARDY_CSV.csv", header = TRUE, stringsAsFactors = FALSE)
 
 ########## Create Corpus and clean data ##########
 ### FOR TWITTER SPECIFIC CLEANING SEE - SENTIMENT ANALYSIS.R 
@@ -33,7 +35,8 @@ input_corpus <- Corpus(VectorSource(input_data$Question))
 
 # tm_map is the main function to do data cleaning. In the below statement
 # Corpus is converted to Plain text.
-input_corpus <- tm_map(input_corpus, PlainTextDocument)
+# input_corpus <- tm_map(input_corpus, PlainTextDocument)
+# Reason for commenting this line of code is given after the code for creating TDM
 
 # Remove unnecessary white spaces
 input_corpus <- tm_map(input_corpus, stripWhitespace)
@@ -57,6 +60,12 @@ tdm <- TermDocumentMatrix(input_corpus,
                                          #, stemming = TRUE
                                         )
                           )
+### Error while creating TDM ###
+### Error in simple_triplet_matrix(i, j, v, nrow = length(terms), ncol = length(corpus),  : 
+### 'i, j' invalid
+# To solve this error the corpus should not be converted to PlainTextDocument by 
+# commenting input_corpus <- tm_map(input_corpus, PlainTextDocument)
+
 #### The below code is commented as everything can be done while creating TDM.
 #### This is just a placeholder to show alternate approach. 
 # # tm_map is the main function to do data cleaning. In the below statement
@@ -120,11 +129,13 @@ individual_words <- names(frequency_of_words)
 
 # Create a data frame with words and frequencies
 word_data_frame <- data.frame(individual_words,frequency_of_words)
+colnames(word_data_frame) = c("Words", "Frequency")
+rownames(word_data_frame) = NULL
 
 # Sorting the data frame on descending order of frequency to understand which words
-# occur more requently. Based on top 100 frequencies, I wil assign the color. Please note 
+# occur more requently. Based on top 100 frequencies, I will assign the color. Please note 
 # that the logic to assign final_color requires manual coding depending on the frequency
-word_data_frame <- word_data_frame[rev(order(word_data_frame$frequency_of_words)),]
+word_data_frame <- word_data_frame[rev(order(word_data_frame$Frequency)),]
 
 ########## Generate the word cloud ########## 
 # Defining the tone depending on the frequency of occurance of the word
@@ -156,8 +167,72 @@ word_data_frame <- word_data_frame[rev(order(word_data_frame$frequency_of_words)
 
 # wordcloud(individual_words, frequency_of_words,  min.freq = 100, scale = c(4,0.5), max.words = 100, color=final_color, ordered.colors=TRUE, random.color=FALSE)
 
-# Using the png command does not show the wordclod in Plots area. The png is saved in current
+# Using the png command does not show the wordcloud in Plots area. The png is saved in current
 # working directory. If dev.off() is not coded then png is not generated unless we close RStudio.
 png("WordCloud.png", width=12, height=8, units="in", res=300)
-wordcloud(individual_words, frequency_of_words,  min.freq = 100, scale = c(4,0.5), max.words = 100, color=brewer.pal(8, "Dark2"), random.order = FALSE)
+# This wordcloud is generated from wordcloud package
+wordcloud(words = word_data_frame$Words
+          , freq = word_data_frame$Frequency
+          , min.freq = 100
+          , scale = c(5, 0.5) # Height of max frequency word = 5 and lowest = 0.5
+          , max.words = 100
+          , color=brewer.pal(8, "Dark2")
+          , random.order = FALSE
+          , rot.per = 0.6 # 60% of words will be rotated
+          )
 dev.off()
+
+########## Generating Word Cloud using Wordcloud2 package ##########
+# While wordcloud2 gives a lot of flexibility on shape of wordcloud, there are a
+# few issues 
+# 1. There is no parameter to restrict the number of words in the wordcloud2; 
+#    similar to max.words parameter. Due to lack of max.words parameter, the 
+#    wordcloud takes a lot of time to generate also. 
+# 2. The wordcloud generated is not a simple plot but an interactive display that 
+#    displays the frequency of the word highlighted by moving cursor hence the 
+#    display can't be easily converted to png.
+
+# In order to circumvent the first issue - create a new data frame with first 100
+# rows of word_data_frame. As word_data_frame is already sorted in descending order
+# of frequency taking first 100 rows will result in wordcloud of top 100 words
+wordcloud2_data_frame = word_data_frame[1:100, ]
+# In order to circumvent the second issue - use webshot and htmlwidgets library. 
+# The widget from wordcloud2 is first stored in an html file in the working 
+# directory using htmlwidgets::saveWidget and then converted to png by taking 
+# screenshot of generated html using webshot::webshot
+library(webshot)
+library(htmlwidgets)
+webshot::install_phantomjs()
+wc = wordcloud2(data = wordcloud2_data_frame
+           , size = 0.8
+           , shape = 'circle'
+           , rotateRatio = 0.5
+           , minSize = 1
+)
+saveWidget(widget = wc
+           , file = "wc.html"
+           , selfcontained = FALSE)
+webshot(url = "wc.html"
+        , file = "wc2.png"
+        , vwidth = 1992
+        , vheight = 1744
+        , delay = 10
+        )
+
+#
+
+# Lettercloud has issues in displaying to RStudio viewer. It is an open issue.
+# https://github.com/ramnathv/htmlwidgets/issues/262
+lc = letterCloud(data = word_data_frame
+            , word = 'Test'
+            , wordSize = 2)
+saveWidget(widget = lc
+           , file = "lc.html"
+           , selfcontained = FALSE)
+webshot(url = "lc.html"
+        , file = "wc2_lc.png"
+        , vwidth = 1992
+        , vheight = 1744
+        , delay = 10
+        )
+# The above is also not working. It is showing blank image. Need to check on that. 
